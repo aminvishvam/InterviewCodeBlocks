718975beac6567e4b9276752eff01f16b6d21752
aminvishvam <vishvamamin55@gmail.com>
2025-04-30 12:48:51 -0400

    Comment out demo class and related code for clarity

--- one.js
+++ one.js
@@ -1,14 +1,27 @@
 
-class demo {
-    constructor(name, age) {
-        this.name = name
-        this.age = age
-    }
+// class demo {
+//     constructor(name, age) {
+//         this.name = name
+//         this.age = age
+//     }
 
-    printName() {
-        return console.log(this.name + this.age)
+//     printName() {
+//         return console.log(this.name + this.age)
+//     }
+// }
+
+// let name = new demo("New", "Name")
+// name.printName();
+
+
+function outerFunction(x) {
+    console.log(x);
+    return function innterFunction(y) {
+        console.log(y);
+        return x + y
     }
 }
 
-let name = new demo("New", "Name")
-name.printName();
+const outerFunction5 = outerFunction(5)
+console.log(outerFunction5)
+console.log(outerFunction5(3));
